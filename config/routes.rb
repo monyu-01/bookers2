5c3d5252a5ff24bf030d97ee920e0e699ce12c51
monyu-01 <opsu.0102@gmail.com>
2025-04-14 08:24:31 +0000

    [add]log inページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get 'home/about', to: 'homes#about', as: 'about'
+
   devise_for :users
   resources :books, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
